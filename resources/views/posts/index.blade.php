402d51dadcde164d081a904a15561d924de374ff
willy836 <wilsonkimiri836@gmail.com>
2023-07-25 16:26:16 +0300

    Generate sample seed data for posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,14 +4,14 @@
 
 @include('partials.messages')
 
-<h2 class="text-center-my-4">All Posts</h2>
-<a href="{{ route('posts.create') }}" class="btn btn-primary">Create Post</a>
+<h2 class="text-center my-4">All Posts</h2>
+<a href="{{ route('posts.create') }}" class="btn btn-primary mb-3">Create Post</a>
 @if (count($posts) > 0)
     @foreach ($posts as $post)
        <article class="card mb-3">
         <div class="card-body">
             <h4>{{ $post->title }}</h4>
-            <p>{{ $post->body }}</p>
+            <p>{{ Str::limit($post->body, 200, '...') }}</p>
             <a href="{{ route('posts.show', $post->id) }}">See Full Post</a>
         </div>
         </article> 
